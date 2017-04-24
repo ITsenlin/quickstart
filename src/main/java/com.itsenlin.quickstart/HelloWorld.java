c1a59cdc0e93cb81fc725c408aaa2896550062ca
itsenlin <wuhebo_86@163.com>
2017-04-24 15:25:19 +0800

    Initial commit

---
+++ src/main/java/com.itsenlin.quickstart/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.itsenlin.quickstart;
+
+/**
+ * Created by itsenlin on 2017/4/24.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
